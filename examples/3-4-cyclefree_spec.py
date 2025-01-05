8a81656cd2bca9f0dd92263a7d0ab6efd83458a7
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-01-05 17:18:32 +0100

    Restore priority-search instead of adjacency-matrix-search

--- examples/3-4-cyclefree_spec.py
+++ examples/3-4-cyclefree_spec.py
@@ -1,50 +1,47 @@
 """Find undirected graphs with many edges that don't contain a 3-cycle or 4-cycle.
 
-On every iteration, improve cyclefree_v1 over the cyclefree_vX methods from previous iterations.
+On every iteration, improve priority_v1 over the priority_vX methods from previous iterations.
 Make only small code-changes. Do not use np.random.
 """
 
 import funsearch
-import numpy as np
 
 
 @funsearch.run
 def evaluate(total_vertex_count: int) -> float:
-  """Returns the average number of edges in an undirected graph on `total_vertex_count` vertices that has no 3-cycles or 4-cycles."""
-  val = len(solve(total_vertex_count))
-  return val
+  """Returns the number of edges in an undirected graph on `total_vertex_count` vertices having no 3-cycles or 4-cycles."""
+  edges = solve(total_vertex_count)
+  return len(edges)
 
 
 def solve(total_vertex_count: int) -> set[tuple[int, int]]:
   """Returns a large graph on `total_vertex_count` vertices without 3-cycles or 4-cycles."""
 
-  graph = cyclefree(total_vertex_count)
-  vertex_to_neighbors = {
-    v: set(
-      w
-      for w in range(total_vertex_count)
-      if (graph[v, w] == 1 or graph[w, v] == 1) and v != w
-    )
-    for v in range(total_vertex_count)
+  edge_to_priority = {
+    (vertex_v, vertex_w): priority(vertex_v, vertex_w, total_vertex_count)
+    for vertex_v in range(total_vertex_count)
+    for vertex_w in range(vertex_v + 1, total_vertex_count)
   }
+  vertex_to_neighbors = {vertex_v: set() for vertex_v in range(total_vertex_count)}
 
-  for vertex_v, vertex_w in [
-    (v, w) for v in range(total_vertex_count) for w in vertex_to_neighbors[v] if w > v
-  ]:
-    # Is the edge {vertex_v, vertex_w} contained in a 3-cycle?
+  for (vertex_v, vertex_w), _ in sorted(edge_to_priority.items(), key=lambda x: -x[1]):
+    # Does adding the edge {vertex_v, vertex_w} add a 3-cycle?
     if not vertex_to_neighbors[vertex_v].isdisjoint(vertex_to_neighbors[vertex_w]):
-      vertex_to_neighbors[vertex_v].remove(vertex_w)
-      vertex_to_neighbors[vertex_w].remove(vertex_v)
       continue
 
-    # Is the edge {vertex_v, vertex_w} contained in a 4-cycle?
+    # Does adding the edge {vertex_v, vertex_w} add a 4-cycle?
+    has_4_cycle = False
     for neighbor_u in vertex_to_neighbors[vertex_v]:
-      if neighbor_u != vertex_w and not vertex_to_neighbors[neighbor_u].isdisjoint(
-        vertex_to_neighbors[vertex_w]
-      ):
-        vertex_to_neighbors[vertex_v].remove(vertex_w)
-        vertex_to_neighbors[vertex_w].remove(vertex_v)
+      if not vertex_to_neighbors[neighbor_u].isdisjoint(vertex_to_neighbors[vertex_w]):
+        has_4_cycle = True
+
         break
+    if has_4_cycle:
+      continue
+
+    # There's no 3-cycle or 4-cycle, so insert the edge
+    vertex_to_neighbors[vertex_v].add(vertex_w)
+    vertex_to_neighbors[vertex_w].add(vertex_v)
 
   edges = set(
     (vertex_v, vertex_w)
@@ -56,9 +53,8 @@ def solve(total_vertex_count: int) -> set[tuple[int, int]]:
 
 
 @funsearch.evolve
-def cyclefree(total_vertex_count: int) -> np.ndarray:
-  """Return the adjacency matrix of an undirected graph on `total_vertex_count` vertices that has many edges but contains no 3-cycles or 4-cycles.
-  The matrix should only have entries 0 and 1.
+def priority(vertex_v: int, vertex_w: int, total_vertex_count: int) -> float:
+  """Returns the priority with which we want to add the undirected edge {vertex_v, vertex_w} to the graph, where vertex_v < vertex_w.
+  `total_vertex_count` is the number of vertices in the graph.
   """
-  matrix = np.zeros((total_vertex_count, total_vertex_count))
-  return matrix
+  return 0.0

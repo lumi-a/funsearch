22097e88cc913e52acb500b7cbcd5a20301873ad
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-01-13 11:56:51 +0100

    Rename gasoline variable

--- funsearch/gasoline/main.py
+++ funsearch/gasoline/main.py
@@ -6,10 +6,10 @@ k = 15
 
 n = 10
 
-uhh = []
+tests = []
 for i in range(100):
   xs = list(np.random.randint(1, 20, size=5))
   ys = list(np.random.randint(1, 20, size=5))
   apx = SlotOrdered().approximation_ratio(xs, ys)
-  uhh.append((xs, ys, apx))
-print(max(uhh, key=lambda x: x[2]))
+  tests.append((xs, ys, apx))
+print(max(tests, key=lambda x: x[2]))

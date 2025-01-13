59c4fadef9892c538e2e816cbc48bb23e6b631de
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-01-13 12:08:32 +0100

    Update paging_spec.py
    - Restrict history to past 1000 items (performance reasons)
    - Stop generating random accesses, read using funsearch.paging instead
    - Clamp index returned from `replace` to {0, ..., num_pages-1}

--- examples/paging_spec.py
+++ examples/paging_spec.py
@@ -6,26 +6,29 @@ Make only small code-changes.
 
 from typing import List
 import funsearch
-from numpy import random
+from funsearch.paging import read_accesses
+from collections import deque
 
 
 @funsearch.run
 def evaluate(_: int) -> float:
   """Returns the (negative) number of page-faults of the paging-problem."""
 
-  accesses = random.randint(1, 100, size=10000)
   num_pages = 30
-  pages = []
+  pages_list = []
+  history = deque(maxlen=1000)
   page_faults = 0
 
-  for i, page in enumerate(accesses):
-    if page not in pages:
-      if len(pages) < num_pages:
-        pages.append(page)
+  for page in read_accesses():
+    if page not in pages_list:
+      if len(pages_list) < num_pages:
+        pages_list.append(page)
       else:
-        index = replace(accesses[:i], pages, page)
-        pages[index] = page
+        index = max(0, min(replace(history, pages_list, page), num_pages - 1))
+        pages_list[index] = page
         page_faults += 1
+
+    history.append(page)
 
   return -page_faults
 
@@ -34,6 +37,6 @@ def evaluate(_: int) -> float:
 def replace(access_history: List[int], pages: List[int], new_page: int) -> int:
   """Given a list of `pages` and a `new_page` not currently in `pages`, return
   the index of the page that `new_page` should replace in `pages`.
-  `access_history` is the list of all page-accesses up to this point, but excluding `new_page`.
+  `access_history` is the list of the past 1000 accesses, but excluding `new_page`.
   """
   return 0

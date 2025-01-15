a66760294dcf4156c2b6056a2977a3500cc6690c
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-01-15 19:23:02 +0100

    Rename variables in 3-4-cyclefree_spec.py

--- examples/3-4-cyclefree_spec.py
+++ examples/3-4-cyclefree_spec.py
@@ -11,42 +11,41 @@ import funsearch
 def evaluate(total_vertex_count: int) -> float:
   """Returns the number of edges in an undirected graph on `total_vertex_count` vertices having no 3-cycles or 4-cycles."""
 
-  fraction = len(solve(total_vertex_count)) / 1192
-  return fraction
+  return len(solve(total_vertex_count))
 
 
 def solve(total_vertex_count: int) -> set[tuple[int, int]]:
   """Returns a large graph on `total_vertex_count` vertices without 3-cycles or 4-cycles."""
 
-  edge_to_priority = {
+  edge_priorities = {
     (vertex_v, vertex_w): priority(vertex_v, vertex_w, total_vertex_count)
     for vertex_v in range(total_vertex_count)
     for vertex_w in range(vertex_v + 1, total_vertex_count)
   }
-  vertex_to_neighbors = {vertex_v: set() for vertex_v in range(total_vertex_count)}
+  neighbors = {vertex_v: set() for vertex_v in range(total_vertex_count)}
 
-  for (vertex_v, vertex_w), _ in sorted(edge_to_priority.items(), key=lambda x: -x[1]):
+  for (vertex_v, vertex_w), _ in sorted(edge_priorities.items(), key=lambda x: -x[1]):
     # Does adding the edge {vertex_v, vertex_w} add a 3-cycle?
-    if not vertex_to_neighbors[vertex_v].isdisjoint(vertex_to_neighbors[vertex_w]):
+    if not neighbors[vertex_v].isdisjoint(neighbors[vertex_w]):
       continue
 
     # Does adding the edge {vertex_v, vertex_w} add a 4-cycle?
     has_4_cycle = False
-    for neighbor_u in vertex_to_neighbors[vertex_v]:
-      if not vertex_to_neighbors[neighbor_u].isdisjoint(vertex_to_neighbors[vertex_w]):
+    for neighbor_u in neighbors[vertex_v]:
+      if not neighbors[neighbor_u].isdisjoint(neighbors[vertex_w]):
         has_4_cycle = True
         break
     if has_4_cycle:
       continue
 
     # There's no 3-cycle or 4-cycle, so insert the edge
-    vertex_to_neighbors[vertex_v].add(vertex_w)
-    vertex_to_neighbors[vertex_w].add(vertex_v)
+    neighbors[vertex_v].add(vertex_w)
+    neighbors[vertex_w].add(vertex_v)
 
   edges = set(
     (vertex_v, vertex_w)
     for vertex_v in range(total_vertex_count)
-    for vertex_w in vertex_to_neighbors[vertex_v]
+    for vertex_w in neighbors[vertex_v]
     if vertex_w > vertex_v
   )
   return edges

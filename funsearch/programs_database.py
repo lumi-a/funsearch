391ffd8fcfb225b35219ab39b91dda0122177a62
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-02-16 20:22:02 +0100

    Add log_tabular to ProgramDatabase

--- funsearch/programs_database.py
+++ funsearch/programs_database.py
@@ -236,6 +236,47 @@ class ProgramsDatabase:
       founder = self._best_program_per_island[founder_island_id]
       founder_scores = self._best_scores_per_test_per_island[founder_island_id]
       self._register_program_in_island(founder, island_id, founder_scores)
+      # TODO: Should we carry over _success_count and _failure_counts?
+
+  def log_tabular(self, first_run=False):
+    scores = self._best_score_per_island
+    score_width = max(5, max(len(str(x)) for x in scores))
+    separator = "  "
+    progression_limit = 6
+
+    output = []
+    headers = [
+      f"{'Isl':>3}",
+      f"{'Score':>{score_width}}",
+      f"{'Queries':>7}",
+      f"{'Failures':>8} {'ok%':>3}",
+    ]
+    output.append(separator.join(headers))
+    output.append("-" * len(headers))
+
+    for idx, score in enumerate(sorted(scores)):
+      successes = self._success_counts[idx]
+      failures = self._failure_counts[idx]
+      attempts = successes + failures
+      success_rate = 100 * successes / attempts if attempts > 0 else 0
+
+      output.append(
+        separator.join(
+          [
+            f"{idx:>3}",
+            f"{score:>{score_width}}",
+            f"{attempts:>7}",
+            f"{failures:>8}",
+            f"{success_rate:>2.0f}%",
+          ]
+        )
+      )
+
+    if not first_run:
+      lines_to_move = len(output)
+      print(f"\033[{lines_to_move}A", end="")
+
+    print("\n".join(output))
 
 
 class Island:

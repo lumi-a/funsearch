92e3330ab249b56d6e060adde13fb3aad9378212
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-02-21 22:06:49 +0100

    Add simple summary containers

--- results/script.js
+++ results/script.js
@@ -12,15 +12,49 @@ async function loadAllData() {
     return datasets
 }
 
-async function displayDatabase(database) {
-    console.log(database)
+function strToPre(str) {
     pre = document.createElement("pre")
     code = document.createElement("code")
     code.classList.add("language-python")
     code.textContent = database[0]["fn"]
     pre.appendChild(code)
     hljs.highlightElement(code)
-    document.body.appendChild(pre)
+    return pre
+}
+
+function problemNameToContainer(problemName) {
+    const maybeExisting = document.getElementById(problemName + "-container")
+    if (maybeExisting) {
+        return maybeExisting
+    }
+
+    const details = document.createElement("details")
+    details.id = problemName + "-container"
+    document.body.appendChild(details)
+
+    const summary = document.createElement("summary")
+    summary.textContent = problemName
+    details.appendChild(summary)
+
+    return details
+}
+
+async function displayDatabase(database) {
+    /*{
+        "config": vars(database._config),  # noqa: SLF001
+        "inputs": database.inputs,
+        "specCode": database._specification,  # noqa: SLF001
+        "failureCounts": database._failure_counts,  # noqa: SLF001
+        "successCounts": database._success_counts,  # noqa: SLF001
+        "bestScorePerIsland": database._best_score_per_island,  # noqa: SLF001
+        "bestProgramPerIsland": [str(p) for p in database._best_program_per_island],  # noqa: SLF001
+        "problemName": database.problem_name,
+        "timestamp": database.timestamp,
+    },*/
+    const problemName = database.problemName
+    const container = problemNameToContainer(problemName)
+
+    // document.body.appendChild(pre)
 }
 
 async function main() {

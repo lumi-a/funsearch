03f72fe82654256ee53f59d3495bb24e5bbcac5f
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-02-23 16:08:40 +0100

    Use color-output in change-db-message

--- funsearch/__main__.py
+++ funsearch/__main__.py
@@ -250,7 +250,16 @@ def change_db_message(db_file: click.File | None) -> None:
   database = ProgramsDatabase.load(db_file)
   old_message = database.message
 
-  database.message = click.edit(old_message)
+  database.message = click.edit(old_message, require_save=False)
+
+  if database.message == old_message:
+    click.echo("Database message unchanged.")
+    db_file.close()
+    return
+  click.echo("Changed message from")
+  click.echo(click.style(old_message, fg="blue"))
+  click.echo("to")
+  click.echo(click.style(database.message, fg="green"))
 
   db_file.seek(0)
   db_file.truncate()

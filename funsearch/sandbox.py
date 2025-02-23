02af0838f0738ef1f2b69081171c13bc26bd1eba
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-02-23 20:48:55 +0100

    Only have ExternalProcessSandbox
    I want to supply the index to the sandbox-calls, so I now want to change as little of the architecture as possible. Besides, I don't understand (and am scared of) the ContainerSandbox.

--- funsearch/sandbox.py
+++ funsearch/sandbox.py
@@ -9,43 +9,8 @@ import cloudpickle
 
 CONTAINER_MAIN = (pathlib.Path(__file__).parent / "container" / "container_main.py").absolute()
 
-IMAGE_NAME = "funsearch_sandbox"
 
-
-class DummySandbox:
-  """Base class for Sandboxes that execute the generated code.
-
-  Note: this base class executes the code but does not offer any sandboxing!!!
-  It should be only used in unit testing or debugging, and not with real LLM
-  unless the host environment is in some kind of sandbox itself.
-  Even in sandboxed host, the executed code could theoretically affect later executions.
-  """
-
-  sandboxes = 0
-
-  def __init__(self, **kwargs) -> None:
-    self.id = DummySandbox.sandboxes
-
-    DummySandbox.sandboxes += 1
-
-  def run(self, program: str, function_to_run: str, test_input, timeout_seconds: int) -> tuple[Any, bool]:
-    """Returns `function_to_run(test_input)` and whether execution succeeded."""
-    # The same "program" seems to be now repeatedly parsed using AST and then compiled.
-    # This could probably be simplified quite a bit.
-    namespace = DummySandbox.compile_code(program)
-    return namespace[function_to_run](test_input)
-
-  @staticmethod
-  def compile_code(program: str):
-    namespace = {}
-
-    parsed_code = ast.parse(program)
-    compiled_code = compile(parsed_code, filename="<ast>", mode="exec")
-    exec(compiled_code, namespace)
-    return namespace
-
-
-class ExternalProcessSandbox(DummySandbox):
+class ExternalProcessSandbox:
   """Sandbox that executes the code in a separate Python process in the same host.
 
   Note: This does not provide real safety and should be only used in an environment where the host process is
@@ -136,67 +101,3 @@ class ExternalProcessSandbox(DummySandbox):
     logging.debug(f"Writing program to {filepath}")
     with open(filepath, "w+") as f:
       f.write(program)
-
-
-class ContainerSandbox(ExternalProcessSandbox):
-  """Basic sandbox that runs unsafe code in Podman or Docker container.
-  - the sandbox should be safe against inadvertent bad code by LLM but not against malicious attacks.
-  - does not require any other dependencies on the host than Podman/Docker
-  - does not support multithreading
-  - might provide easier or more lightweight debugging experience than some other fancier sandbox environments.
-  """
-
-  executable = "podman"
-  image_built = False
-
-  @classmethod
-  def build_image(cls, extra_pip_packages) -> None:
-    version = sys.version.split(" ")[0]
-    ret = os.system("podman --version")
-    if ret != 0:
-      ret = os.system("docker --version")
-      if ret != 0:
-        msg = "Could not find Podman or Docker. Can not use ContainerSandbox."
-        raise Exception(msg)
-      cls.executable = "docker"
-
-    dockerfile = pathlib.Path(__file__).parent / "container" / "Dockerfile"
-    logging.debug("Building container image")
-    extra = ""
-    if extra_pip_packages:
-      extra = f'--build-arg INSTALL_PACKAGES="{extra_pip_packages}"'
-
-    cmd = (
-      f"{cls.executable} build --build-arg PYTHON_VERSION={version} {extra} "
-      f"-t {IMAGE_NAME} -f {dockerfile} {CONTAINER_MAIN.parent}"
-    )
-    logging.debug(f"Executing: {cmd}")
-    os.system(cmd)
-    cls.image_built = True
-
-  def __init__(self, base_path: pathlib.Path, extra_pip_packages: str = "numpy", timeout_secs=30) -> None:
-    super().__init__(base_path, timeout_secs)
-
-    if not ContainerSandbox.image_built:
-      ContainerSandbox.build_image(extra_pip_packages)
-
-  def _exec(self, call_data_path: pathlib.Path, input_path: pathlib.Path, error_file_path: pathlib.Path):
-    """Use podman/docker to execute python in a container.
-    - The main.py shall execute the LLM generated method from prog.pickle file providing
-      input.pickle as the input for the method.
-    - main.py writes the output of the method into output.pickle.
-    Everything except the /workspace folder will be read-only so that the environment remains good
-    for future runs.
-    """
-    cmd = (
-      f"{self.executable} run "
-      f"--stop-timeout={self.timeout_secs} "
-      f"-v {CONTAINER_MAIN}:/main.py:ro "
-      f"-v {call_data_path}:/workspace "
-      f"-v {input_path}:/input.pickle:ro "
-      f"{IMAGE_NAME}:latest /usr/local/bin/python3 "
-      f"/main.py /workspace/prog.pickle /input.pickle /workspace/output.pickle"
-      f"  2> {error_file_path}"
-    )
-    logging.debug(f"Executing: {cmd}")
-    return os.system(cmd)

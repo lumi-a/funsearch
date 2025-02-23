4c751038573f85f9e654a69da36171ac0155930b
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-02-23 19:12:51 +0100

    Reduce the number of samples per prompt
    Leads to more diverse prompts. Should increase diversity of responses, while hopefully not incurring too high a performance-cost.

--- funsearch/config.py
+++ funsearch/config.py
@@ -64,4 +64,4 @@ class Config:
   programs_database: ProgramsDatabaseConfig = dataclasses.field(default_factory=ProgramsDatabaseConfig)
   num_samplers: int = 15
   num_evaluators: int = 1
-  samples_per_prompt: int = 4
+  samples_per_prompt: int = 1

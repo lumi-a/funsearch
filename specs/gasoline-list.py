27a576e79a6c8e56dc793d9a26245545d9492387
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-03-09 18:52:04 +0100

    Truncate gasoline-lists

--- specs/gasoline-list.py
+++ specs/gasoline-list.py
@@ -1,10 +1,11 @@
-"""I'm trying to find sets for which an iterative rounding algorithm on the gasoline-problem has a poor (high) approximation-ratio.
+"""I'm trying to find instances of the gasoline-problem for which an iterative rounding algorithm has a poor (high) approximation-ratio.
 
 The gasoline-problem looks for a permutation of the xs and ys (lists of positive integers) such that maximum of the differences of prefix-sums is as small as possible, i.e. maximum_(m,n) zs[n]-zs[m] is as as small as possible, where zs[n] = xs[0] - ys[0] + xs[1] - ys[1] + ... + xs[n//2] - (ys[n] if n is odd else 0).
 
 To generate sets with poor approximation-ratios, I have tried the following functions so far. Please write another one that is similar and has the same signature, but has some lines altered slightly.
 """
 
+import math
 import funsearch
 
 
@@ -19,23 +20,24 @@ def evaluate(n: int) -> float:
     if (xs, ys) != gasoline(n):
         return 0
 
-    xs = [max(0, min(2**31 - 1, int(x))) for x in xs]
-    ys = [max(0, min(2**31 - 1, int(y))) for y in ys]
+    xs = [max(0, min(2**31 - 1, int(x))) for x in xs[:n]]
+    ys = [max(0, min(2**31 - 1, int(y))) for y in ys[:n]]
 
     return SlotOrdered().approximation_ratio(xs, ys)
 
 
 @funsearch.evolve
 def gasoline(n: int) -> tuple[list[int], list[int]]:
-    """Return a new gasoline-problem, specified by the list of x-values and y-values.
-    The integers will be clamped to [0, 2**31 - 1].
+    """Return a new gasoline-problem, specified by the list of x-values and y-values,
+    each of which must have length at most `n`.
     """
+    k = int(math.log2(n + 2)) - 1
     xs, ys = [], []
-    for i in range(1, n):
-        u = int(2**n * (1 - 2 ** (-i)))
+    for i in range(1, k):
+        u = int(2**k * (1 - 2 ** (-i)))
         xs.extend([u for _ in range(2**i)])
         ys.extend([u for _ in range(2**i)])
-    xs.extend([int(2**n) for _ in range(2**n)])
-    u = int(2**n * (1 - 2 ** (-n)))
-    ys.extend([u for _ in range(2**n)])
+    xs.extend([int(2**k) for _ in range(2**k)])
+    u = int(2**k * (1 - 2 ** (-k)))
+    ys.extend([u for _ in range(2**k)])
     return xs, ys

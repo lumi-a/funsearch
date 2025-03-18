7ae10a96d38d00a17592f117f048bbaa933599ab
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-03-18 10:16:18 +0100

    Add price_of_greedy

--- specs/clustering-rs/src/lib.rs
+++ specs/clustering-rs/src/lib.rs
@@ -1,14 +1,18 @@
+use ndarray::prelude::*;
+use price_of_hierarchy::{Cost, KMeans};
 use pyo3::prelude::*;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn price_of_kmeans_greedy(points: Vec<Vec<f64>>) -> PyResult<f64> {
+    let points: Vec<Array1<f64>> = points.into_iter().map(Array1::from_vec).collect();
+    let ratio = KMeans::new(&points).price_of_greedy().0;
+    Ok(ratio)
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn clustering_rs(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(price_of_kmeans_greedy, m)?)?;
     Ok(())
 }

85954f39581ee102fe9a6a458ed5b16d7574dfc0
lumi-a <151774547+lumi-a@users.noreply.github.com>
2025-08-28 14:11:21 +0200

    Improve identical_checker.py

--- identical_checker.py
+++ identical_checker.py
@@ -15,16 +15,17 @@ DigestedClusters = dict[Signature, set[int]]  # Only stores hashes
 DigestedIslands = list[DigestedClusters]
 
 if __name__ == "__main__":
-    # Map from config-string to (name, digested_islands)
+    # Map from config-string to (name, sample-count, digested_islands)
     identities: defaultdict[str, list[tuple[str, DigestedIslands]]] = defaultdict(list)
     for backup in Path("data/backups").glob("*.pickle"):
         database = ProgramsDatabase.load(backup.open("rb"))
         config_str = str(database._config)
+        samples = sum(len(island._runs) for island in database._islands)
         digested_islands = [
             {k: set(hash(str(program)) for program in v._programs) for k, v in island._clusters.items()}
             for island in database._islands
         ]
-        identities[config_str].append((backup.name, digested_islands))
+        identities[config_str].append((backup.name, samples, digested_islands))
 
     def sub_digestion(a: DigestedIslands, b: DigestedIslands) -> bool:
         if len(a) != len(b):
@@ -42,14 +43,16 @@ if __name__ == "__main__":
 
     for items in identities.values():
         non_subsets = []
-        for ix_a, (a_name, a_digest) in enumerate(items):
+        for ix_a, (a_name, a_samples, a_digest) in enumerate(items):
             was_subset = False
-            for b_name, b_digest in items[ix_a + 1 :]:
+            for b_name, _, b_digest in items[ix_a + 1 :]:
                 if sub_digestion(a_digest, b_digest):
                     print(f"{a_name} is a subset of {b_name}")
                     was_subset = True
                     break
             if not was_subset:
-                non_subsets.append(a_name)
+                non_subsets.append((a_name, a_samples))
         if len(non_subsets) > 1:
-            print(f"All have the same config, but are not subsets of each other: {non_subsets}")
+            print(f"All have the same config, but are not subsets of each other:")
+            for name, samples in non_subsets:
+                print(f"   {samples} samples: {name}")
